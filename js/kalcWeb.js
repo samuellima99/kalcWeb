554080b5f2b3d98dd8c3beafd1638af0153dc7d7
samuellima99 <lima300799@gmail.com>
2019-08-14 16:50:52 -0300

    Prova PWEB 2.

--- js/kalcWeb.js
+++ js/kalcWeb.js
@@ -1,37 +1,138 @@
+//Definido os operadores
 
-//funçõa para capturar valor
+let operadores = ["+", "-", "x", "÷", "."];
+
+//Definindo variaveis auxiliares
+
+let valorAtual = 0
+let valorPassado = 0
+let resultado = 0
+let status = false
+let operador
+
+
+// Captura o estado da tela
+
+const tela = document.querySelector(".screen")
+
+//função para capturar valores
 
 function capturaValor(valor) {
-    exibirNaTela(valor)
+    exibirNoDisplay(valor)
 }
 
-//função de mostrar na tela
+//função para capturar os operadores
 
-function exibirNaTela(valor) {
-    const tela = document.querySelector(".screen")
-    tela.innerText += valor
+function capturaOperador(operacao) {
+
+    operador = operacao
+
+    valorPassado = resultado
+    valorAtual = tela.innerText
+
+    switch (operador) {
+        case operadores[0]:
+            tela.innerText = ""
+            resultado = soma(valorPassado, valorAtual)
+            exibirNoDisplay(resultado)
+            status = true
+            break;
+        case operadores[1]:
+            tela.innerText = ""
+            if (valorPassado == '0') {
+                resultado = subtracao(valorAtual, valorPassado)
+                exibirNoDisplay(resultado)
+                status = true
+            } else {
+                resultado = subtracao(valorPassado, valorAtual)
+                exibirNoDisplay(resultado)
+                status = true
+            }
+            break;
+        case operadores[2]:
+            tela.innerText = ""
+            if (valorPassado == '0') {
+                resultado = multiplicacao(valorAtual, 1)
+                exibirNoDisplay(resultado)
+                status = true
+            } else {
+                resultado = multiplicacao(valorAtual, valorPassado)
+                exibirNoDisplay(resultado)
+                status = true
+            }
+            break;
+        case operadores[3]:
+            tela.innerText = ""
+            if (valorPassado == '0' || valorPassado <= '0') {
+                resultado = divisao(valorAtual, 1)
+                exibirNoDisplay(resultado)
+                status = true
+            } else {
+                resultado = divisao(valorPassado, valorAtual)
+                exibirNoDisplay(resultado)
+                status = true
+            }
+            break;
+    }
 }
 
-//função captura operador
+//função igual  
 
-function capturaOperador(operador) {
-    exibirNaTela(operador)
+function igual(params) {
+    capturaOperador(operador)
+    if (operador != "") {
+        operador = ""
+    }
 }
 
-//função de mostrar o operador na tela
+//função para exibir no display
 
-function exibirOperador(operador) {
-    const tela = document.querySelector(".screen")
-    tela.innerText += operador
+function exibirNoDisplay(valor) {
+    if (status) {
+        status = false;
+        tela.innerText = ""
+        tela.innerText = valor
+    } else {
+        if (valor == operadores[4] && tela.innerText == "0") {
+            tela.innerText = "0"
+            tela.innerText += valor
+        } else if(tela.innerText == "0") {
+            tela.innerText = ""
+            tela.innerText += valor
+        } else {
+            tela.innerText += valor
+        }
+    }
 }
 
 //função de limpar a tela
 
-function limpaTela(params) {
-    const clear = document.querySelector(".screen")
-    clear.innerText = '0'
+function limpaTela() {
+    tela.innerText = "0"
+    valorPassado = 0
+    status = false
+    valorAtual = 0
+    resultado = 0
+    operador = 0
 }
 
+//funções de soma, divisão, multiplicação e subtração
+
+function soma(valor01, valor02) {
+    return Number(valor01) + Number(valor02)
+}
+
+function subtracao(valor01, valor02) {
+    return Number(valor01) - Number(valor02)
+}
+
+function multiplicacao(valor01, valor02) {
+    return Number(valor01) * Number(valor02)
+}
+
+function divisao(valor01, valor02) {
+    return Number(valor01) / Number(valor02)
+}
 
 
 
